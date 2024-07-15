dce2da8e4359de3709a2e8951452d760c17ccad7
UroojAsad <uroojshehzadi82@gmail.com>
2024-07-15 15:08:17 -0700

    my second app

---
+++ android/app/src/main/kotlin/com/project/mysecondapplication/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.project.mysecondapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+
+          title: Text( 'I am RICH',
+style: TextStyle(
+              color: Colors.white
+          ),
+        ),
+
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+
+      ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png')
+          ),
+        ),
+    )
+  )
+  );
+}
+
